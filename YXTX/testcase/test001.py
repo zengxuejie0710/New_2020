a5db3db8c35cc087c254ec62880d4855bbdb3e5a
zengxj <zengxj@ymhz-tech.com>
2020-09-02 15:34:48 +0800

    第四次提交

--- YXTX/testcase/test001.py
+++ YXTX/testcase/test001.py
@@ -16,33 +16,29 @@ class TestSearchReservation:
 
     def setup(self):
         print('* * * Start * * *')
-        self.app = App()
+        self.app = App().start()
 
     def teardown(self):
         print('* * * End * * *')
-        # self.app.stop()
-
-    # @pytest.mark.parametrize("start,end", [("ds","sss"),("12"),("22")])
-    # def test01(self,start,end):
-    #     print('ceshi',start,end)
+        self.app.stop()
 
 
     @pytest.mark.parametrize("start,end",testdata)
     def test01(self,start,end):
-        first_step = self.app.start().goto_index()
+        first_step = self.app.goto_index()
         first_step.isexit_order()
         first_step.deparstaion(start, end)
         origin_stop = first_step.verity_start()
         arrival_stop = first_step.verity_end()
-        # amount, serve = first_step.getsum()
-        # first_step.now_pay()
-        # first_step.order_context()
-        # adress = first_step.is_adress()
-        # phone = first_step.is_phone()
-        # result = first_step.cancal_order()
+        amount, serve = first_step.getsum()
+        first_step.now_pay()
+        first_step.order_context()
+        adress = first_step.is_adress()
+        phone = first_step.is_phone()
+        result = first_step.cancal_order()
         assert (origin_stop)
         assert (arrival_stop)
-        # assert (amount == serve)
-        # assert (adress)
-        # assert (phone)
-        # assert (result)
+        assert (amount == serve)
+        assert (adress)
+        assert (phone)
+        assert (result)
